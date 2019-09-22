228920b66490d53b5944f558db01ab7348a373b7
ishikado <okazakisaburo@gmail.com>
2019-09-22 23:07:59 +0900

    連結リストのenumを追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
+// リスト表現
+enum List{
+    Cons(Types, Box<List>),
+    Nil
+}
+
+// 許容する型一覧
+enum Types{
+    Int(i32),
+    Atom(String),
+    List(Box<List>)
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        use super::List;
+        use super::Types;
+
+        let _test = List::Cons(Types::Int(32), Box::new(List::Cons(Types::Atom("a".to_string()), Box::new(List::Nil))));
+        let _test2 = List::Cons(Types::List(Box::new(List::Nil)), Box::new(List::Nil));
+
+
         assert_eq!(2 + 2, 4);
     }
 }
