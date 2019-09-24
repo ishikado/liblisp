eb170eb1510b123858323bdb3bdcbf40be6a42de
takeshi sugiyama <takeshi_sugiyama@dwango.co.jp>
2019-09-24 13:15:24 +0900

    to_type関数の実装。テスト追加。

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,11 @@ mod lisp {
         LispList(Box<LispList>),
     }
 
+    #[derive(Debug, Clone, PartialEq)]
+    pub enum TypeConversionError {
+        InvalidToken
+    }
+
     // リスト操作を行う関数
     impl LispList {
         pub fn new() -> LispList {
@@ -59,39 +64,38 @@ mod lisp {
     }
 
     // 文字列を受け取り、Type形式に変換する関数
-    pub fn to_type(bytes: &[u8]) -> Type {
+    // TODO Type::from(String) など、Typeに文字列からTypeを生成する関数を impl したい
+    pub fn to_type(bytes: &[u8]) -> Result<Type, TypeConversionError> {
         let mut index = 0;
         return to_type_(&mut index, bytes);
     }
 
-    fn to_type_(index: &mut usize, bytes: &[u8]) -> Type {
-        // :TODO 実装する
-        // list or atom or int
-        // atomは かんたんのために、alphabetから始まり、alphabetと数字のみ含むものとする
+    fn to_type_(index: &mut usize, bytes: &[u8]) -> Result<Type, TypeConversionError> {
         let head_ch = char::from(bytes[*index]);
         let mut list = LispList::new();
         // list
         if head_ch == '(' {
             *index += 1;
             loop {
-                let result = to_type_(index, bytes);
-                list = list.cons(&result);
-
                 // spaceを飛ばす
                 while *index < bytes.len() && char::from(bytes[*index]) == ' ' {
                     *index += 1;
                 }
 
+                // 終端判定
                 if *index == bytes.len() {
                     // TODO : error handling
                     panic!("occured unexpected error");
                 } else if char::from(bytes[*index]) == ')' {
                     // end
+                    *index += 1;
                     // TODO : reverse list
-                    return Type::LispList(Box::new(list));
-                } else {
-                    continue;
-                }
+                    return Ok(Type::LispList(Box::new(list)));
+                }                
+                
+                // 新しい要素を追加
+                let result = to_type_(index, bytes)?;
+                list = list.cons(&result);
             }
         }
         // int
@@ -103,49 +107,56 @@ mod lisp {
                     // unwrapしているが、直前のif文で数字かどうかを判定しているので panic は発生しない
                     num = num * 10 + c.to_digit(10).unwrap() as i32;
                 } else {
-                    // 括弧以外の文字が続いていたら異常
-                    if c != ')' {
-                        // TODO erro handling
-                        panic!("occured unexpected error");
+                    // 括弧 or space 以外の文字が続いていたら異常
+                    if !(c == ')' || c == ' ') {
+                        return Err(TypeConversionError::InvalidToken);
                     }
                     break;
                 }
+                *index += 1;
             }
-            return Type::Int(num);
+            return Ok(Type::Int(num));
         }
         // atom
+        // atomは 簡単のために、alphabetから始まり、alphabetと数字のみ含むものとする
         else if head_ch.is_alphabetic() {
             let mut atom = "".to_string();
             while *index < bytes.len() {
                 let c = char::from(bytes[*index]);
-                if c.is_ascii_digit() {
-                    // unwrapしているが、直前のif文で数字かどうかを判定しているので panic は発生しない
-                    // num = num * 10 + c.to_digit(10).unwrap() as i32;
-
-                    // TODO implementation
-
-                } else if c.is_alphabetic() {
-                    // TODO implementation
-
+                if c.is_ascii_digit() || c.is_alphabetic() {
+                    atom.push(c);
                 } else {
-                    // 括弧以外の文字が続いていたら異常
-                    if c != ')' {
-                        // TODO erro handling
-                        panic!("occured unexpected error");
+                    // 括弧 or space 以外の文字が続いていたら異常
+                    if !(c == ')' || c == ' ') {
+                        return Err(TypeConversionError::InvalidToken);
                     }
                     break;
                 }
+                *index += 1;
             }
-            return Type::Atom(Box::new(atom));
+            return Ok(Type::Atom(Box::new(atom)));
         }
-
-        return Type::Int(0);
+        return Err(TypeConversionError::InvalidToken);
     }
 
 }
 
 #[cfg(test)]
 mod tests {
+    #[test]
+    fn to_type_tests() {
+        use super::lisp::*;
+        
+        assert_eq!(to_type("12345".as_bytes()), Ok(Type::Int(12345)));
+        assert_eq!(to_type("atom".as_bytes()), Ok(Type::Atom(Box::new("atom".to_string()))));
+        assert_eq!(to_type("atom123".as_bytes()), Ok(Type::Atom(Box::new("atom123".to_string()))));
+        assert_eq!(to_type("123atom".as_bytes()), Err(TypeConversionError::InvalidToken));
+        assert_eq!(to_type("( )".as_bytes()), Ok(Type::LispList(Box::new(LispList::Nil))));
+        assert_eq!(to_type("( ( ) )".as_bytes()), Ok(Type::LispList(
+            Box::new(LispList::Cons(Type::LispList(Box::new(LispList::Nil)), Box::new(LispList::Nil))))));
+        
+    }
+
     #[test]
     fn lisplist_tests() {
         use super::lisp::LispList;
