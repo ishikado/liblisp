54d4124f685e1960826eaed87a490244aab92574
ishikado <okazakisaburo@gmail.com>
2019-10-01 01:08:59 +0900

    統合テスト用のディレクトリ tests を追加# Please enter the commit message for your changes. Lines starting

---
+++ tests/lib.rs
@@ -0,0 +1,11 @@
+extern crate liblisp;
+
+
+#[test]
+fn integration_test() {
+    // TODO: 実装する
+    // モジュール内のテストだけだと、lib.rs の内容のうち、 pub をつけなかったものまで公開されてしまうため、pub つけ忘れに気が付かない
+    // そのため、ここで、公開インターフェース全体に対するテストを書くようにしたい
+
+    assert!(true);
+}
